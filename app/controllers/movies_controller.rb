1588e5ff4000444b1f018c78d02d9a883434c058
CHEMAMOCTEZUMA <jmoctezu@chicagobooth.edu>
2015-10-27 23:38:33 -0500

    Just missing styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  
+
   def director
 
   	@list_of_directors = Director.all
@@ -9,6 +9,33 @@ class MoviesController < ApplicationController
   def director_bio
 
   	@director = Director.find_by({:id => params[:id]})
+
+  end
+
+  def edit_director
+
+	@director = Director.find_by({:id => params["id"]})
+
+  end
+
+  def update_director
+
+	@update_director = Director.find_by({:id => params["id"]})
+  	@update_director.name = params[:name]
+  	@update_director.image_url = params[:image_url]
+  	@update_director.bio = params[:bio]
+  	@update_director.dob = params[:dob]
+  	@update_director.save	
+
+  	redirect_to("http://localhost:3000/directors")
+
+  end  
+  
+  def destroy_director
+
+	@delete_director = Director.find_by({:id => params[:id]})
+	@name = @delete_director.name
+	@delete_director.destroy
 
   end
 
@@ -24,6 +51,33 @@ class MoviesController < ApplicationController
 
   end
 
+  def edit_actor
+
+	@actor = Actor.find_by({:id => params["id"]})
+
+  end
+
+  def update_actor
+
+	@update_actor = Actor.find_by({:id => params["id"]})
+  	@update_actor.name = params[:name]
+  	@update_actor.image_url = params[:image_url]
+  	@update_actor.bio = params[:bio]
+  	@update_actor.dob = params[:dob]
+  	@update_actor.save	
+
+  	redirect_to("http://localhost:3000/actors")
+
+  end  
+
+  def destroy_actor
+
+	@delete_actor = Actor.find_by({:id => params[:id]})
+	@name = @delete_actor.name
+	@delete_actor.destroy
+
+  end
+
   def movie
 
   	@movies = Movie.all
@@ -33,6 +87,34 @@ class MoviesController < ApplicationController
   def movie_det
 
   	@movie = Movie.find_by({:id => params[:id]})
+
+  end
+
+  def edit_movie
+
+	@movie = Movie.find_by({:id => params["id"]})
+
+  end
+
+  def update_movie
+
+	@update_movie = Movie.find_by({:id => params["id"]})
+  	@update_movie.title = params[:title]
+  	@update_movie.image_url = params[:image_url]
+  	@update_movie.year = params[:year]
+  	@update_movie.duration = params[:duration]
+  	@update_movie.description = params[:description]
+  	@update_movie.save	
+
+  	redirect_to("http://localhost:3000/movies")
+
+  end  
+
+  def destroy_movie
+
+	@delete_movie = Movie.find_by({:id => params[:id]})
+	@name = @delete_movie.title
+	@delete_movie.destroy
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,44 @@
 Rails.application.routes.draw do
  
+get("/", { :controller => "movies", :action => "movie" })
+
+
+#directors
+
 get("/directors", { :controller => "movies", :action => "director" })
 
 get("/directors/:id", { :controller => "movies", :action => "director_bio" })
+
+get("/directors/delete_director/:id", { :controller => "movies", :action => "destroy_director" })
+
+get("/directors/:id/edit", { :controller => "movies", :action => "edit_director" })
+
+get("/directors/update_director/:id", { :controller => "movies", :action => "update_director" })
+
+
+#actors
 
 get("/actors", { :controller => "movies", :action => "actor" })
 
 get("/actors/:id", { :controller => "movies", :action => "actor_bio" })
 
+get("/actors/delete_actor/:id", { :controller => "movies", :action => "destroy_actor" })
+
+get("/actors/:id/edit", { :controller => "movies", :action => "edit_actor" })
+
+get("/actors/update_actor/:id", { :controller => "movies", :action => "update_actor" })
+
+
+#movies
+
 get("/movies", { :controller => "movies", :action => "movie" })
 
 get("/movies/:id", { :controller => "movies", :action => "movie_det" })
+
+get("/movies/delete_movie/:id", { :controller => "movies", :action => "destroy_movie" })
+
+get("/movies/:id/edit", { :controller => "movies", :action => "edit_movie" })
+
+get("/movies/update_movie/:id", { :controller => "movies", :action => "update_movie" })
 
 end
